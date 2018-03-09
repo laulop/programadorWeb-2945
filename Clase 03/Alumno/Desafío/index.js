91f86a1424f6b8aee2658a9558d39f1389ebe373
Lau Lop <lopezlaura023@hotmail.com>
2018-03-09 20:42:09 -0300

    Desafío resuelto

---
+++ Clase 03/Alumno/Desafío/index.js
@@ -0,0 +1,103 @@
+/*-------------------------------DESAFIO 1 - CLASE03---------------------------*/
+/*askOperation()
+
+function sum (number1, number2) {
+  return number1 + number2
+}
+
+function res (number1, number2) {
+  return number1 - number2
+}
+
+function mul (number1, number2) {
+  return number1 * number2
+}
+
+function div (number1, number2) {
+  if (number2 != 0) {
+  return number1 / number2
+} else {
+  console.log('División por cero, no permitido!')
+ }
+}
+
+function parsear(number1, number2) {
+  return parseInt(number1, number2)
+
+
+/*-----------------------------DESAFIO 2 - CLASE03 -----------------------------*/
+
+
+askOperation()
+
+function sum (number1, number2) {
+  return number1 + number2
+}
+
+function res (number1, number2) {
+  return number1 - umber2
+}
+
+function mul (number1, number2) {
+  return number1 * number2
+}
+
+function div (number1, number2) {
+  return number1 / number2
+}
+
+function askAndParse (order) {
+  var number1 = prompt('Ingerese el ' + order + ' número')
+  var parsedNumber1 = parseInt(number1, 10)
+  while (isNaN(parsedNumber1)) {
+    console.log('Número erroneo')
+    var number1 = prompt('Ingerese el ' + order + ' número')
+    var parsedNumber1 = parseInt(number1, 10)
+  }
+  return parsedNumber1
+}
+
+function askOperation () {
+  var operation = prompt('Ingrese la operación a realizar, sum, res, mul o div')
+  var result
+  switch (operation) {
+    case 'sum':
+      var number1 = askAndParse('primer')
+      var number2 = askAndParse('segundo')
+      result = sum(number1, number2)
+      showResult(operation, result)
+      break
+    case 'res':
+      var number1 = askAndParse('primer')
+      var number2 = askAndParse('segundo')
+      result = res(number1, number2)
+      showResult(operation, result)
+      break
+    case 'mul':
+      var number1 = askAndParse('primer')
+      var number2 = askAndParse('segundo')
+      result = mul(number1, number2)
+      showResult(operation, result)
+      break
+    case 'div':
+      var number1 = askAndParse('primer')
+      do {
+        var number2 = askAndParse('segundo')
+      } while (number2 === 0)
+
+      result = div(number1, number2)
+      showResult(operation, result)
+      break
+    default:
+      showError(operation)
+      break
+  }
+}
+
+function showResult (operation, result) {
+  console.log('El resultado de la ' + operation + ' es: ' + result)
+}
+
+function showError (operation) {
+  console.log(operation + ' no es una operación valida')
+}
