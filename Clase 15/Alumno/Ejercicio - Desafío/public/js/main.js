e0cf09f713dfe94cf8f26b909f0ec4d6276226ce
Lau Lop <lopezlaura023@hotmail.com>
2018-04-21 15:36:28 -0300

    actualizamos

---
+++ Clase 15/Alumno/Ejercicio - Desafío/public/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+            $('#home').click(function () {
+                $('#root').load('../partials/home.html')
+
+            })
+
+            $('#people').click(function () {
+                    $('#root').load('../partials/people.html'
+                    })
+
+                $('#contact').click(function () {
+                        $('#root').load('../partials/contact.html'
+                        })
+                })
